4addeff71f4f650f75ff2f8da8fd2e2642cc26a3
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-06-13 23:16:00 +0300

    Create mergef.ts

---
+++ mergef.ts
@@ -0,0 +1,30 @@
+function mergeSort(arr: number[]): number[] {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left: number[], right: number[]): number[] {
+    let resultArray: number[] = [], leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            resultArray.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            resultArray.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const array = [34, 7, 23, 32, 5, 62];
+console.log(mergeSort(array));
